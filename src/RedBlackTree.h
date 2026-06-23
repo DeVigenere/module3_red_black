43d3b03a4406e443b353eb3e3b988b136aa6814a
DeVigenere <161169792+DeVigenere@users.noreply.github.com>
2026-06-23 21:37:03 +0300

    v0.2

--- src/RedBlackTree.h
+++ src/RedBlackTree.h
@@ -9,82 +9,72 @@ class RedBlackTree {
     struct Node {
         T data;
         Color color;
-        std::shared_ptr<Node> left;
-        std::shared_ptr<Node> right;
-        std::weak_ptr<Node> parent;
-        Node(const T& value) : data(value), color(Color::RED), left(nullptr), right(nullptr) {};
+        std::unique_ptr<Node> left;
+        std::unique_ptr<Node> right;
+        Node* parent;
+        Node(const T& value) : data(value), color(Color::RED), left(nullptr), right(nullptr), parent(nullptr) {};
     };
-    std::shared_ptr<Node> root;
-    std::shared_ptr<Node> nil;
+    std::unique_ptr<Node> root;
 
-    void initialization_nil() {
-        nil = std::make_shared<Node>(T());
-        nil->color = Color::BLACK;
-        nil->left = nullptr;
-        nil->right = nullptr;
-    }
-
-    void leftRotate(std::shared_ptr<Node> x) {
-        auto y = x->right;
-        x->right = y->left;
-        if (y->left != nil) {
-            y->left->parent = x;
+    void leftRotate(Node* x) {
+        auto y = x->right.get();
+        x->right = std::move(y->left);
+        if (x->right) {
+            x->right->parent = x;
         }
         y->parent = x->parent;
-        auto parent = x->parent.lock();
+        auto parent = x->parent;
         if (parent == nullptr) {
-            root = y;
+            root.reset(y);
         }
-        else if (x == parent->left) {
-            parent->left = y;
+        else if (x == parent->left.get()) {
+            parent->left.reset(y);
         }
         else {
-            parent->right = y;
+            parent->right.reset(y);
         }
-        y->left = x;
+        y->left.reset(x);
         x->parent = y;
     }
 
-    void rightRotate(std::shared_ptr<Node> y) {
-        auto x = y->left;
-        y->left = x->right;
-        if (x->right != nil) {
-            x->right->parent = y;
+    void rightRotate(Node* y) {
+        auto x = y->left.get();
+        y->left = std::move(x->right);
+        if (y->left) {
+            y->left->parent = y;
         }
         x->parent = y->parent;
-        auto parent = y->parent.lock();
+        auto parent = y->parent;
         if (parent == nullptr) {
-            root = x;
+            root.reset(x);
         }
-        else if (y == parent->left) {
-            parent->left = x;
+        else if (y == parent->left.get()) {
+            parent->left.reset(x);
         }
         else {
-            parent->right = x;
+            parent->right.reset(x);
         }
-        x->right = y;
+        x->right.reset(y);
         y->parent = x;
     }
 
-    void insertFixup(std::shared_ptr<Node> z) {
-        auto parent = z->parent.lock();
-        while (parent && parent->color == Color::RED) {
-            auto grandparent = parent->parent.lock();
-            if (parent == grandparent->left) {
-                auto uncle = grandparent->right;
-                if (uncle->color == Color::RED) {
+    void insertFixup(Node* z) {
+        while (z != root.get() && z->parent->color == Color::RED) {
+            auto parent = z->parent;
+            auto grandparent = parent->parent;
+            if (parent == grandparent->left.get()) {
+                auto uncle = grandparent->right.get();
+                if (uncle && uncle->color == Color::RED) {
                     parent->color = Color::BLACK;
                     uncle->color = Color::BLACK;
                     grandparent->color = Color::RED;
                     z = grandparent;
-                    parent = z->parent.lock();
                 }
                 else {
-                    if (z == parent->right) {
+                    if (z == parent->right.get()) {
                         z = parent;
                         leftRotate(z);
-                        parent = z->parent.lock();
-                        grandparent = parent->parent.lock();
+                        parent = z->parent;
                     }
                     parent->color = Color::BLACK;
                     grandparent->color = Color::RED;
@@ -93,20 +83,18 @@ class RedBlackTree {
                 }
             }
             else {
-                auto uncle = grandparent->left;
-                if (uncle->color == Color::RED) {
+                auto uncle = grandparent->left.get();
+                if (uncle && uncle->color == Color::RED) {
                     parent->color = Color::BLACK;
                     uncle->color = Color::BLACK;
                     grandparent->color = Color::RED;
                     z = grandparent;
-                    parent = z->parent.lock();
                 }
                 else {
-                    if (z == parent->left) {
+                    if (z == parent->left.get()) {
                         z = parent;
                         rightRotate(z);
-                        parent = z->parent.lock();
-                        grandparent = parent->parent.lock();
+                        parent = z->parent;
                     }
                     parent->color = Color::BLACK;
                     grandparent->color = Color::RED;
@@ -118,75 +106,78 @@ class RedBlackTree {
         root->color = Color::BLACK;
     }
 
-    void deleteFixup(std::shared_ptr<Node> x) {
-        while (x != root && x->color == Color::BLACK) {
-            auto parent = x->parent.lock();
-            if (x == parent->left) {
-                auto w = parent->right;
+    void deleteFixup(Node* x, Node* parent) {
+        while (x != root.get() && (x == nullptr || x->color == Color::BLACK)) {
+            if (x == parent->left.get() || (x == nullptr && parent->left == nullptr)) {
+                auto w = parent->right.get();
                 if (w->color == Color::RED) {
                     w->color = Color::BLACK;
                     parent->color = Color::RED;
                     leftRotate(parent);
-                    w = parent->right;
+                    w = parent->right.get();
                 }
-                if (w->left->color == Color::BLACK && w->right->color == Color::BLACK) {
+                if ((w->left == nullptr || w->left->color == Color::BLACK) &&
+                    (w->right == nullptr || w->right->color == Color::BLACK)) {
                     w->color = Color::RED;
                     x = parent;
+                    parent = x->parent;
                 }
                 else {
-                    if (w->right->color == Color::BLACK) {
-                        w->left->color = Color::BLACK;
+                    if (w->right == nullptr || w->right->color == Color::BLACK) {
+                        if (w->left) w->left->color = Color::BLACK;
                         w->color = Color::RED;
                         rightRotate(w);
-                        w = parent->right;
+                        w = parent->right.get();
                     }
                     w->color = parent->color;
                     parent->color = Color::BLACK;
-                    w->right->color = Color::BLACK;
+                    if (w->right) w->right->color = Color::BLACK;
                     leftRotate(parent);
-                    x = root;
-                    break;
+                    x = root.get();
+                    parent = nullptr;
                 }
             }
             else {
-                auto w = parent->left;
+                auto w = parent->left.get();
                 if (w->color == Color::RED) {
                     w->color = Color::BLACK;
                     parent->color = Color::RED;
                     rightRotate(parent);
-                    w = parent->left;
+                    w = parent->left.get();
                 }
-                if (w->right->color == Color::BLACK && w->left->color == Color::BLACK) {
+                if ((w->right == nullptr || w->right->color == Color::BLACK) &&
+                    (w->left == nullptr || w->left->color == Color::BLACK)) {
                     w->color = Color::RED;
                     x = parent;
+                    parent = x->parent;
                 }
                 else {
-                    if (w->left->color == Color::BLACK) {
-                        w->right->color = Color::BLACK;
+                    if (w->left == nullptr || w->left->color == Color::BLACK) {
+                        if (w->right) w->right->color = Color::BLACK;
                         w->color = Color::RED;
                         leftRotate(w);
-                        w = parent->left;
+                        w = parent->left.get();
                     }
                     w->color = parent->color;
                     parent->color = Color::BLACK;
-                    w->left->color = Color::BLACK;
+                    if (w->left) w->left->color = Color::BLACK;
                     rightRotate(parent);
-                    x = root;
-                    break;
+                    x = root.get();
+                    parent = nullptr;
                 }
             }
         }
-        x->color = Color::BLACK;
+        if (x) x->color = Color::BLACK;
     }
 
-    std::shared_ptr<Node> findNode(const T& value) const {
-        auto current = root;
-        while (current != nil && current != nullptr) {
+    Node* findNode(const T& value) const {
+        auto current = root.get();
+        while (current) {
             if (value < current->data) {
-                current = current->left;
+                current = current->left.get();
             }
             else if (value > current->data) {
-                current = current->right;
+                current = current->right.get();
             }
             else {
                 return current;
@@ -195,112 +186,141 @@ class RedBlackTree {
         return nullptr;
     }
 
-    std::shared_ptr<Node> minimum(std::shared_ptr<Node> node) const {
-        while (node->left != nil) {
-            node = node->left;
+    Node* minimum(Node* node) const {
+        while (node->left) {
+            node = node->left.get();
         }
         return node;
     }
 
-    void transplant(std::shared_ptr<Node> u, std::shared_ptr<Node> v) {
-        auto parent = u->parent.lock();
+    void transplant(Node* u, std::unique_ptr<Node>&& v) {
+        auto parent = u->parent;
         if (parent == nullptr) {
-            root = v;
+            root = std::move(v);
+            if (root) root->parent = nullptr;
         }
-        else if (u == parent->left) {
-            parent->left = v;
+        else if (u == parent->left.get()) {
+            parent->left = std::move(v);
+            if (parent->left) parent->left->parent = parent;
         }
         else {
-            parent->right = v;
+            parent->right = std::move(v);
+            if (parent->right) parent->right->parent = parent;
         }
-        v->parent = parent;
     }
 
-    void inorderTraversal(std::shared_ptr<Node> node, std::ostream& os) const {
-        if (node != nil && node != nullptr) {
-            inorderTraversal(node->left, os);
+    std::unique_ptr<Node> extractNode(Node* node) {
+        auto parent = node->parent;
+        std::unique_ptr<Node> result;
+        if (parent == nullptr) {
+            result = std::move(root);
+        }
+        else if (node == parent->left.get()) {
+            result = std::move(parent->left);
+        }
+        else {
+            result = std::move(parent->right);
+        }
+        return result;
+    }
+
+    void inorderTraversal(Node* node, std::ostream& os) const {
+        if (node) {
+            inorderTraversal(node->left.get(), os);
             os << node->data << " ";
-            inorderTraversal(node->right, os);
+            inorderTraversal(node->right.get(), os);
         }
     }
 
 public:
-    RedBlackTree() {
-        initialization_nil();
-        root = nil;
+    RedBlackTree() : root(nullptr) {}
+
+    void insert(const T& value) {
+        auto z = std::make_unique<Node>(value);
+        Node* zPtr = z.get();
+        Node* y = nullptr;
+        auto x = root.get();
+
+        while (x) {
+            y = x;
+            if (zPtr->data < x->data) {
+                x = x->left.get();
+            }
+            else {
+                x = x->right.get();
+            }
+        }
+
+        zPtr->parent = y;
+        if (y == nullptr) {
+            root = std::move(z);
+        }
+        else if (zPtr->data < y->data) {
+            y->left = std::move(z);
+            y->left->parent = y;
+        }
+        else {
+            y->right = std::move(z);
+            y->right->parent = y;
+        }
+
+        insertFixup(zPtr);
     }
 
     void remove(const T& value) {
         auto z = findNode(value);
-        if (!z || z == nil) return;
-        auto y = z;
-        auto yOriginalColor = y->color;
-        std::shared_ptr<Node> x;
-        if (z->left == nil) {
-            x = z->right;
-            transplant(z, z->right);
+        if (!z) return;
+
+        Node* y = z;
+        Color yOriginalColor = y->color;
+        Node* x = nullptr;
+        Node* xParent = nullptr;
+
+        if (z->left == nullptr) {
+            x = z->right.get();
+            xParent = z->parent;
+            auto zPtr = extractNode(z);
+            transplant(z, std::move(zPtr->right));
+            if (x) x->parent = xParent;
         }
-        else if (z->right == nil) {
-            x = z->left;
-            transplant(z, z->left);
+        else if (z->right == nullptr) {
+            x = z->left.get();
+            xParent = z->parent;
+            auto zPtr = extractNode(z);
+            transplant(z, std::move(zPtr->left));
+            if (x) x->parent = xParent;
         }
         else {
-            y = minimum(z->right);
+            y = minimum(z->right.get());
             yOriginalColor = y->color;
-            x = y->right;
-            auto parent = y->parent.lock();
-            if (parent == z) {
-                x->parent = y;
+            x = y->right.get();
+
+            if (y->parent == z) {
+                xParent = y;
+                if (x) x->parent = y;
             }
             else {
-                transplant(y, y->right);
-                y->right = z->right;
-                y->right->parent = y;
+                xParent = y->parent;
+                auto yPtr = extractNode(y);
+                transplant(y, std::move(yPtr->right));
+                y->right = std::move(z->right);
+                if (y->right) y->right->parent = y;
             }
-            transplant(z, y);
-            y->left = z->left;
-            y->left->parent = y;
+
+            auto zPtr = extractNode(z);
+            transplant(z, std::move(zPtr));
+            y->left = std::move(z->left);
+            if (y->left) y->left->parent = y;
             y->color = z->color;
         }
+
         if (yOriginalColor == Color::BLACK) {
-            deleteFixup(x);
+            deleteFixup(x ? x : root.get(), xParent ? xParent : root.get());
         }
-    }
-
-    void insert(const T& value) {
-        auto z = std::make_shared<Node>(value);
-        z->left = nil;
-        z->right = nil;
-
-        std::shared_ptr<Node> y = nullptr;
-        auto x = root;
-
-        while (x != nil && x != nullptr) {
-            y = x;
-            if (z->data < x->data) {
-                x = x->left;
-            }
-            else {
-                x = x->right;
-            }
-        }
-
-        z->parent = y;
-        if (y == nullptr) {
-            root = z;
-        }
-        else if (z->data < y->data) {
-            y->left = z;
-        }
-        else {
-            y->right = z;
-        }
-
-        insertFixup(z);
     }
 
     void print() const {
-        inorderTraversal(root, std::cout);
+        inorderTraversal(root.get(), std::cout);
         std::cout << std::endl;
     }
 
@@ -311,5 +331,4 @@ public:
         }
         return std::nullopt;
     }
-
 };
